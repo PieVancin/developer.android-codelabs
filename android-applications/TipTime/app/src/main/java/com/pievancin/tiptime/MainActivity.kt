d893adb0a59c7b39a8a5f55776cf04e61a056519
Pierre <pie.vancin@gmail.com>
2021-04-17 23:28:44 -0300

    refactor to view binding

--- android-applications/TipTime/app/src/main/java/com/pievancin/tiptime/MainActivity.kt
+++ android-applications/TipTime/app/src/main/java/com/pievancin/tiptime/MainActivity.kt
@@ -2,10 +2,15 @@ package com.pievancin.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.pievancin.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
